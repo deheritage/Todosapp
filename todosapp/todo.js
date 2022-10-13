73da7b8f267e292afb5d0cb61ad1e3d6fe4f15ca
Akinbinu Arike <akinbinuarike2020@gmail.com>
2022-10-12 22:25:48 -0700

    first commit

--- todosapp/todo.js
+++ todosapp/todo.js
@@ -1,53 +1,85 @@
 console.log("todoList app is active here");
-const todoList = [];
 
-const removeItem = (itemNumber) => {
-  document.getElementById("taskList").innerHTML = "";
-  todoList.splice(itemNumber, 1);
-  addTodo();
-};
+const getTodos = () => {
+    const todos = localStorage.getItem('todoList');
+    return todos ? JSON.parse(todos) : [];
+}
+
+const todoList = getTodos();
+
+const setTodos = (todos) => {
+localStorage.setItem('todoList', JSON.stringify(todos)
+);
+}
+
 
 //create a function to add an item to todoList
 
 const addTodo = (value) => {
   if (value) {
-    todoList.push(value);
-  }
-  // console.log("got here");
+    todoList.push({id: Date.now(), value:value, isCompleted: false});
+    setTodos(todoList);
 
+
+  }
+  document.getElementById("inputName").value = "";
+  refreshDom();
+};
+
+const editItem = (itemNumber) => {
+  let editedItem = prompt("Edit Todo", todoList[itemNumber].value);
+  
+  if (editedItem) {
+    todoList[itemNumber].value = editedItem;
+  setTodos(todoList);
+}
+refreshDom();
+}
+
+const removeItem = (itemNumber) => {
+  todoList.splice(itemNumber, 1);
+setTodos(todoList);
+
+  refreshDom();
+};
+
+const editIsCompleted = (itemNumber) => {
+if (itemNumber > -1) {
+  todoList[itemNumber].isCompleted = !todoList[itemNumber].isCompleted;
+  
+}
+refreshDom();
+}
+
+const refreshDom=()=>{
+  document.getElementById("taskList").innerHTML = "";
+  console.log(todoList);
   todoList.forEach((item, index) => {
     const nameList = document.getElementById("taskList");
-    const list = document.createElement("li");
+    const newClassName = item.isCompleted ? 'isCompleted' : '';
+    console.log(newClassName);
+    const list =
+      "<li><p class='"+newClassName+"' onclick={editIsCompleted(" +
+      index +
+      ")} >" +
+      item.value +
+      "</p> <div><button onclick={removeItem(" +
+      index +
+      ")}><i class='fa fa-trash'></i></button><button onclick={editItem(" +
+      index +
+      ")}><i class='fa fa-edit'></i></button></div></li>";
 
-    const deleteButton = document.createElement("button");
-    deleteButton.setAttribute("id", item);
-    deleteButton.setAttribute("onclick", "removeItem(" + index + ")");
-    deleteButton.appendChild(document.createTextNode("delete"));
-    console.log(deleteButton);
-
-    const todoText = document.createElement("p");
-    todoText.appendChild(document.createTextNode(item));
-    console.log(todoText);
-
-    //create a text node from the users input
-    list.appendChild(todoText);
-    list.appendChild(deleteButton);
 
     //adds the users textnode at the end of the list
-    nameList.appendChild(list);
+    nameList.innerHTML += list;
+    
   });
-
-  document.getElementById("inputName").value = "";
-};
+}
 //add an addEventListener to click the button
+refreshDom();
 
 document.querySelector("#myBtn").addEventListener("click", () => {
-  //empty the ul child element
-  document.getElementById("taskList").innerHTML = "";
   //add value in the input text property...
   const inputValue = document.getElementById("inputName").value;
-
   addTodo(inputValue);
-  console.log("todoList", todoList);
-  console.log("button clicked");
 });
